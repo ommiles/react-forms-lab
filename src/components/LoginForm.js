3ce2d326f0b4da3776195d4fca8412d922efac13
OM Miles <olivia.m.miles@gmail.com>
2021-05-02 18:02:00 -0700

    LoginForm complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,31 +1,55 @@
 import React from "react";
 
-import React from "react";
-
 class LoginForm extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  state = {
+    username: '',
+    password: ''
   }
 
+  handleInput = (e) => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value,
+      }, () => console.log(this.state)
+    )
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    e.target.reset()
+    return this.props.handleLogin(this.state)
+  }
+  
   render() {
     return (
-      <form>
+      <form 
+      onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            onChange={this.handleInput}
+            required />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            onChange={this.handleInput}
+            required />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button 
+          type="submit">Log in</button>
         </div>
       </form>
     );
